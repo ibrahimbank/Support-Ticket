0e83a3fdc38d7d14c9d7720b6ca6e56381a48f5b
bankole <bankoleibrahim96@gmail.com>
2022-02-15 20:38:01 +0100

    created single listing of ticket

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,5 +1,62 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getTicket, reset } from "../features/ticket/ticketSlice";
+import BackButton from "../component/BackButton";
+import Spinner from "../component/Spinner";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+
 function Ticket() {
-  return <div>Ticket</div>;
+  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.ticket
+  );
+
+  const params = useParams();
+
+  const dispatch = useDispatch();
+
+  const { ticketId } = useParams();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    dispatch(getTicket(ticketId));
+    // eslint-disable-next-line
+  }, [isError, message, ticketId]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h3>Something went wrong</h3>;
+  }
+
+  return (
+    <div className="ticket-page">
+      <div className="ticket-header">
+        <BackButton url="/tickets" />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString("en-US")}
+        </h3>
+        <hr />
+
+        <div className="ticket-desc">
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Ticket;
